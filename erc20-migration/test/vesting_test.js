17019d1a5111406658e40e1153dcb8ba8ff3d80e
NisticoAn <nistic.andrea@gmail.com>
2025-04-09 15:49:08 +0200

    develop test helpers

--- erc20-migration/test/vesting_test.js
+++ erc20-migration/test/vesting_test.js
@@ -1,5 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+const { hrtime } = require("process");
+
 
 describe("Vesting test", function () {
 
@@ -9,7 +12,7 @@ describe("Vesting test", function () {
   let TIME_BETWEEN_INTERVALS = 1000;
   let INTERVALS_TO_CLAIM = 20;
   let AMOUNT_EACH_CLAIM = 10;
-  let START_TIMESTAMP = 100;
+  let startTimestamp;
 
   beforeEach(async function () {
     beneficiary = (await ethers.getSigners())[0].address;
@@ -21,15 +24,44 @@ describe("Vesting test", function () {
     await erc20.deploymentTransaction().wait();
 
     //deploy vesting
+    startTimestamp = await time.latest() + 10;
     factory = await ethers.getContractFactory("LinearTokenVesting");
-    vesting = await factory.deploy(await erc20.getAddress(), beneficiary, AMOUNT_EACH_CLAIM, START_TIMESTAMP, TIME_BETWEEN_INTERVALS, INTERVALS_TO_CLAIM);
+    vesting = await factory.deploy(await erc20.getAddress(), beneficiary, AMOUNT_EACH_CLAIM, startTimestamp, TIME_BETWEEN_INTERVALS, INTERVALS_TO_CLAIM);
     await vesting.deploymentTransaction().wait();
 
     //mint
     await erc20.mint(await vesting.getAddress(), AMOUNT_EACH_CLAIM*INTERVALS_TO_CLAIM);
   });
 
-  it("TODO", async function () {
+  async function _assertBalance(expectedBalance) {
+    let balance = await erc20.balanceOf(beneficiary);
+    expect(balance).to.be.equal(expectedBalance);
+  }
+
+  async function _setTimetampAndClaim(claimTimestamp) {
+    await time.setNextBlockTimestamp(claimTimestamp);
+    await vesting.claim();
+  }
+
+  async function _setTimetampAndClaimFails(claimTimestamp, errorName) {
+    await time.setNextBlockTimestamp(claimTimestamp);
+    expect(vesting.claim()).to.be.revertedWithCustomError(vesting, errorName);
+  }
+
+  it("TODO claim fails if no period has passed", async function () {
+    await time.setNextBlockTimestamp(startTimestamp+1);
+    expect(true).to.be.equal(true);
+  });
+
+  it("TODO claim success for one period in between, then a second period", async function () {
+    expect(true).to.be.equal(true);
+  });
+
+  it("TODO claim success for all periods, then fails", async function () {
+    expect(true).to.be.equal(true);
+  });
+
+  it("TODO claim after one period, then claim success if multiple periods has passed", async function () {
     expect(true).to.be.equal(true);
   });
 });
